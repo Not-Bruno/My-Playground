c9f81c8a8fe105a53428fb75b6ed4481b5b670a9
Bruno <brunoscholz@gmx.de>
2023-02-22 12:48:33 +0100

    modified:   src/server/backup-controller.py

--- src/server/backup-controller.py
+++ src/server/backup-controller.py
@@ -1,1 +1,19 @@
-pint("Hello")
+import pcapy
+
+def print_packet(packet):
+    print(f"Received a packet: {packet}")
+
+devs = pcapy.findalldevs()
+print(f"Available devices: {devs}")
+
+# Öffne das erste Interface
+cap = pcapy.open_live(devs[0], 65536, True, 100)
+
+# Setze den Filter auf eingehende Pakete
+cap.setfilter('inbound')
+
+# Schleife, um eingehende Pakete zu verarbeiten
+while True:
+    (header, packet) = cap.next()
+    if packet:
+        print_packet(packet)
