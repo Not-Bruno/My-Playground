13a5f5034e4e9f6539069cf871802ced79f683df
Bruno <brunoscholz@gmx.de>
2023-02-22 18:14:52 +0100

    Update backup-controller.py

--- src/server/backup-controller.py
+++ src/server/backup-controller.py
@@ -1,19 +1,31 @@
-import pcapy
+import socket
+import struct
 
-def print_packet(packet):
-    print(f"Received a packet: {packet}")
+def packet_to_string(packet):
+    eth_header = packet[:14]
+    eth = struct.unpack('!6s6sH', eth_header)
+    eth_protocol = socket.ntohs(eth[2])
+    
+    if eth_protocol == 8:
+        ip_header = packet[14:34]
+        iph = struct.unpack('!BBHHHBBH4s4s', ip_header)
+        protocol = iph[6]
+        s_addr = socket.inet_ntoa(iph[8])
+        d_addr = socket.inet_ntoa(iph[9])
+        return f"Protocol: {protocol}, Source Address: {s_addr}, Destination Address: {d_addr}"
+    else:
+        return ""
 
-devs = pcapy.findalldevs()
-print(f"Available devices: {devs}")
+def main():
+    with open('network_traffic.log', 'w') as f:
+        conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.ntohs(3))
+        while True:
+            packet, _ = conn.recvfrom(65535)
+            log = packet_to_string(packet)
+            if log:
+                print(log)
+                f.write(log + '\n')
 
-# Öffne das erste Interface
-cap = pcapy.open_live(devs[0], 65536, True, 100)
+if __name__ == '__main__':
+    main()
 
-# Setze den Filter auf eingehende Pakete
-cap.setfilter('inbound')
-
-# Schleife, um eingehende Pakete zu verarbeiten
-while True:
-    (header, packet) = cap.next()
-    if packet:
-        print_packet(packet)
